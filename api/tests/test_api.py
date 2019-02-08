3368b70588e36c7a57fa08fd2b2602e210b23729
Sullivan MATAS <sullivan@izberg-marketplace.com>
2019-02-08 22:42:12 +0100

    Add test for GET /api/task/:id route

--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -85,3 +85,37 @@ class APITestCase(TestCase):
             "test_loss": "0.542310", # Feature 6 decimals
             "test_accuracy": "0.746200", # Feature 6 decimals
         }])
+
+    def test_get_task(self):
+        training_configuration = self.get_training_configuration()
+
+        task = TrainingTask.objects.create(
+            training_configuration=training_configuration,
+            status=TrainingTask.COMPLETE,
+            failure_message="This is a test.",
+            started_on=datetime.datetime(
+                2019, 1, 16, 19, 15, 4, 1, tzinfo=utc
+            ),
+            terminated_on=datetime.datetime(
+                2019, 1, 16, 19, 17, 23, 2, tzinfo=utc
+            ),
+            test_loss=Decimal("0.54231"),
+            test_accuracy=Decimal("0.7462"),
+        )
+
+        response = self.client.get('/api/task/{}'.format(task.id))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {
+            "training_configuration": training_configuration.pk,
+            "status": TrainingTask.COMPLETE,
+            "created_on": task.created_on.isoformat().replace(
+                    '00:00', '0000'
+                ),
+            "started_on": alter_tz_format(task.started_on.isoformat()),
+            "terminated_on": alter_tz_format(task.terminated_on.isoformat()),
+            "id": task.id,
+            "failure_message": "This is a test.",
+            "test_loss": "0.542310", # Feature 6 decimals
+            "test_accuracy": "0.746200", # Feature 6 decimals
+        })
