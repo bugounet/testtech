657b6e5c66bd59eef7314e4f2b080bb39586c29e
Sullivan MATAS <sullivan@izberg-marketplace.com>
2019-02-08 17:43:18 +0100

    Add test expecting we can get a list of TrainingConfigurations.

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
 
 # Create your tests here.
+class APITestCase(SimpleTestCase):
+    def test_list_trainings(self):
+        response = self.client.get("/api/trainings")
+
+        self.assertJSONEqual(response, [{
+            "algorithm": 'algo1.py',
+            "id": "1",
+            "dockerfile": 'Dockerfile.dms',
+            "created_on": '2019-01-01T00:00:00',
+        }])
