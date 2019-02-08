d2f6c0c19bfde0c9c3db48185fd4916eb6cdfeb8
Sullivan MATAS <sullivan@izberg-marketplace.com>
2019-02-08 18:43:55 +0100

    Create django-admin class view for creating, reading and accessing configs

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from api.models import TrainingConfiguration
+
+
+@admin.register(TrainingConfiguration)
+class TrainingConfigurationAdmin(admin.ModelAdmin):
+    model = TrainingConfiguration
+    list_display = ('id', 'created_on', 'algorithm_name', )
+
+    def algorithm_name(self, instance):
+        return instance.algorithm.name if instance.algorithm else ""
