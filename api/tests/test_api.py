04ddb69971bf988a04574320d566ed26700d29f6
Sullivan MATAS <sullivan@izberg-marketplace.com>
2019-02-08 22:38:45 +0100

    Move tests to a package to keep each test suite into a dedicated file.

--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -1,8 +1,16 @@
+import datetime
 from decimal import Decimal
 from django.test import TestCase
 
 # Create your tests here.
+from pytz import utc
+
 from api.models import TrainingConfiguration
+from api.models import TrainingTask
+
+
+def alter_tz_format(datetime_str):
+    return datetime_str.replace("+00:00", "+0000")
 
 
 class APITestCase(TestCase):
@@ -35,9 +43,7 @@ class APITestCase(TestCase):
             "id": training_configuration.id,
             "dockerfile": 'Dockerfile.dms',
             "created_on":
-                training_configuration.created_on.isoformat().replace(
-                    '00:00', '0000'
-                ),
+                alter_tz_format(training_configuration.created_on.isoformat())
         })
 
     def get_training_configuration(self):
@@ -51,8 +57,14 @@ class APITestCase(TestCase):
 
         task = TrainingTask.objects.create(
             training_configuration=training_configuration,
-            status="training",
+            status=TrainingTask.COMPLETE,
             failure_message="This is a test.",
+            started_on=datetime.datetime(
+                2019, 1, 16, 19, 15, 4, 1, tzinfo=utc
+            ),
+            terminated_on=datetime.datetime(
+                2019, 1, 16, 19, 17, 23, 2, tzinfo=utc
+            ),
             test_loss=Decimal("0.54231"),
             test_accuracy=Decimal("0.7462"),
         )
@@ -60,14 +72,16 @@ class APITestCase(TestCase):
         response = self.client.get('/api/tasks')
 
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(task.json(), [{
+        self.assertEqual(response.json(), [{
             "training_configuration": training_configuration.pk,
-            "status": "training",
+            "status": TrainingTask.COMPLETE,
             "created_on": task.created_on.isoformat().replace(
                     '00:00', '0000'
                 ),
+            "started_on": alter_tz_format(task.started_on.isoformat()),
+            "terminated_on": alter_tz_format(task.terminated_on.isoformat()),
             "id": task.id,
             "failure_message": "This is a test.",
-            "test_loss": "0.54231",
-            "test_accuracy": "0.7462",
+            "test_loss": "0.542310", # Feature 6 decimals
+            "test_accuracy": "0.746200", # Feature 6 decimals
         }])
