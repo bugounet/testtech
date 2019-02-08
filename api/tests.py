468b85562e8f954a7402a818111457464042b1ae
Sullivan MATAS <sullivan@izberg-marketplace.com>
2019-02-08 19:04:28 +0100

    Refactor test to put test-model creation in a function

--- api/tests.py
+++ api/tests.py
@@ -6,11 +6,10 @@ from api.models import TrainingConfiguration
 
 class APITestCase(TestCase):
     def test_list_trainings(self):
-        training_configuration = TrainingConfiguration.objects.create(
-            algorithm='algo1.py',
-            dockerfile='Dockerfile.dms',
-        )
+        training_configuration = self.get_training_configuration()
+
         response = self.client.get("/api/trainings")
+
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), [{
             "algorithm": 'algo1.py',
@@ -23,13 +22,12 @@ class APITestCase(TestCase):
         }])
 
     def test_get_single_training(self):
-        training_configuration = TrainingConfiguration.objects.create(
-            algorithm='algo1.py',
-            dockerfile='Dockerfile.dms',
-        )
+        training_configuration = self.get_training_configuration()
+
         response = self.client.get(
             "/api/training/{}".format(training_configuration.id)
         )
+
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), {
             "algorithm": 'algo1.py',
@@ -40,3 +38,9 @@ class APITestCase(TestCase):
                     '00:00', '0000'
                 ),
         })
+
+    def get_training_configuration(self):
+        return TrainingConfiguration.objects.create(
+            algorithm='algo1.py',
+            dockerfile='Dockerfile.dms',
+        )
