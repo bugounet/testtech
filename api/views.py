57a7e9b24b55675ebf02ecfe162eec258d3b4561
Sullivan MATAS <sullivan@izberg-marketplace.com>
2019-02-08 18:08:46 +0100

    Create basic API returning mocked model

--- api/views.py
+++ api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+import json
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
 
 # Create your views here.
+class TrainingConfigurationListView(APIView):
+    def get(self, request, *args, **kwargs):
+        return Response([
+            {
+                'algorithm': 'algo1.py',
+                'dockerfile': 'Dockerfile.dms',
+                'id': '1',
+                'created_on': '2019-01-01T00:00:00'
+            }
+        ])
